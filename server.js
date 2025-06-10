77d9e679cbc268c1fba54104dce8d0aaf8e2e939
Makobyfull <Jose_r_licona12@hotmail.com>
2025-06-10 20:29:28 +0200

    modifiche

--- server.js
+++ server.js
@@ -25,11 +25,11 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin) || /\.vercel\.app$/.test(origin)) {
+    if (!origin || allowedOrigins.includes(origin) || /^https:\/\/.*\.vercel\.app$/.test(origin)) {
       return callback(null, true);
     }
     callback(new Error('Not allowed by CORS'));
-  },
+  },  
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
